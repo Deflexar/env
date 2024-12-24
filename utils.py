b6febb384d478d1f9a249c4ea642c1f6d5eb0f61
Deflexar <121299777+Deflexar@users.noreply.github.com>
2024-12-24 12:07:48 +0300

    Create utils.py
    create method for read .env file.

---
+++ utils.py
@@ -0,0 +1,9 @@
+def env():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(base_dir, '.env')
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.strip() and not line.startswith('#'):
+                key, value = line.strip().split('=', 1)
+                os.environ[key.strip()] = value.strip()
